80d32d2a505dd5f8fd41ce1fa8736e9c20d40d8a
tangshaoneng <3038956605@qq.com>
2021-03-04 22:31:24 +0800

    这是第一次作业

---
+++ homework1/Calculator/Calculator/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入第一个运算数：");
+            string first_num;
+            float first_num1;
+            while (true)
+            {
+                first_num = Console.ReadLine();
+                try
+                {
+                 first_num1 = Convert.ToSingle(first_num);
+            }
+            catch (Exception)
+            {
+                    Console.WriteLine("您输入的不是数字，请重新输入");
+                continue;
+            }
+                break;
+            }
+            Console.WriteLine("请输入第二个运算数：");
+            string second_num;
+            float second_num1;
+            while (true)
+            {
+                second_num = Console.ReadLine();
+                try
+                {
+                 second_num1 = Convert.ToSingle(second_num);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("您输入的不是数字，请重新输入");
+                    continue;
+                }
+                break;
+            }
+            Console.WriteLine("请输入运算符（仅支持加减乘除模）");
+            while (true) {
+                string Calculation = Console.ReadLine();
+                if (Calculation == "+"){
+                float result = first_num1 + second_num1;
+                Console.WriteLine(result);
+                    break;
+            }
+
+            else if (Calculation == "-")
+            {
+                float result = first_num1 + second_num1;
+                Console.WriteLine(result);
+                    break;
+            }
+
+            else if (Calculation == "*")
+            {
+                float result = first_num1 * second_num1;
+                Console.WriteLine(result);
+                    break;
+            }
+
+            else if (Calculation == "/")
+            {
+                float result = first_num1 / second_num1;
+                Console.WriteLine(result);
+                    break;
+            }
+
+            else if (Calculation == "%")
+            {
+                float result = first_num1 % second_num1;
+                Console.WriteLine(result);
+                    break;
+            }
+
+            else
+            {
+                    Console.WriteLine("您输入的不是一个合法的运算符，请重新输入");
+                    continue;
+            }
+            }
+
+        }
+    }
+}
---
+++ homework1/WinFormsCalculator/WinFormsCalculator/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WinFormsCalculator;
+
+namespace WinFormsCalculator
+{
+    public partial class Form1 : Form
+    {
+        string operator_;
+        string String = "";
+        float[] numbers = new float[2];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            number_add(button1.Text);
+            textBox1.Text = String;
+            textBox1.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            number_add(button4.Text);
+            textBox1.Text = String;
+            textBox1.Show();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            number_add(button5.Text);
+            textBox1.Text = String;
+            textBox1.Show();
+        }
+        private void number_add(string num)
+        {
+            String += num;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            number_add(button2.Text);
+            textBox1.Text = String;
+            textBox1.Show();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            number_add(button7.Text);
+            textBox1.Text = String;
+            textBox1.Show();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            number_add(button9.Text);
+            textBox1.Text = String;
+            textBox1.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            number_add(button3.Text);
+            textBox1.Text = String;
+            textBox1.Show();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            number_add(button10.Text);
+            textBox1.Text = String;
+            textBox1.Show();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            number_add(button8.Text);
+            textBox1.Text = String;
+            textBox1.Show();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            numbers[0] = Convert.ToSingle(String);
+            operator_ = button6.Text;
+            String = "";
+            textBox1.Text = String;
+            textBox1.Show();
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            number_add(button18.Text);
+            textBox1.Text = String;
+            textBox1.Show();
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            number_add(button17.Text);
+            textBox1.Text = String;
+            textBox1.Show();
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            numbers[0] = Convert.ToSingle(String);
+            operator_ = button14.Text;
+            String = "";
+            textBox1.Text = String;
+            textBox1.Show();
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            numbers[0] = Convert.ToSingle(String);
+            operator_ = button11.Text;
+            String = "";
+            textBox1.Text = String;
+            textBox1.Show();
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            numbers[0] = Convert.ToSingle(String);
+            operator_ = button15.Text;
+            String = "";
+            textBox1.Text = String;
+            textBox1.Show();
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            numbers[0] = Convert.ToSingle(String);
+            operator_ = button12.Text;
+            String = "";
+            textBox1.Text = String;
+            textBox1.Show();
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            numbers[1] = Convert.ToSingle(String);
+            if (operator_ == "+")
+            {
+                textBox1.Text = (numbers[0] + numbers[1]).ToString();
+                textBox1.Show();
+                String = "";
+            }
+
+            if (operator_ == "-")
+            {
+                textBox1.Text = (numbers[0] - numbers[1]).ToString();
+                textBox1.Show();
+                String = "";
+            }
+
+            if (operator_ == "*")
+            {
+                textBox1.Text = (numbers[0] * numbers[1]).ToString();
+                textBox1.Show();
+                String = "";
+            }
+
+            if (operator_ == "/")
+            {
+                textBox1.Text = (numbers[0] / numbers[1]).ToString();
+                textBox1.Show();
+                String = "";
+            }
+
+            if (operator_ == "%")
+            {
+                textBox1.Text = (numbers[0] % numbers[1]).ToString();
+                textBox1.Show();
+                String = "";
+            }
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            String = String.Substring(0, String.Length - 1);
+            textBox1.Text = String;
+            textBox1.Show();
+        }
+    }
+}
