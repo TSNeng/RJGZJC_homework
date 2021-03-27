fae60f57ec168e19d0bb7e0bacdb6d44a458ea94
tlssnp <70431735+tlssnp@users.noreply.github.com>
2021-03-27 16:18:17 +0800

    forth_homework

---
+++ homework4/Clock/Clock/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Timers;
+using System.Threading;
+
+namespace Clock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("请设置闹钟的响铃时间(以hh:mm:ss格式设置");
+            string clock_time;
+            
+            clock_time = Console.ReadLine();
+            
+            Clock clock = new Clock();
+            Alarm alarm = new Alarm(clock);
+            Tick tick = new Tick(clock);
+            Thread thread_1 = new Thread(clock.On);
+            thread_1.Start();
+            clock.On2(clock_time);
+        }
+    }
+
+    delegate void Handler();
+    delegate void Handler2(string clock_time);
+
+    class Clock
+    {
+        public string time = DateTime.Now.ToString("hh:mm:ss");
+        public event Handler Func;
+        public event Handler2 Func1;
+        public void On()
+        {
+            Func();
+        }
+
+        public void On2(string clock_time)
+        {
+            Func1(clock_time);
+        }
+    }
+
+    class Tick
+    {
+        public Tick(Clock clock)
+        {
+            clock.Func += tick;
+        }
+
+        public void tick()
+        {
+            while (true)
+            {
+                Console.WriteLine(DateTime.Now.ToString("hh:mm:ss"));
+                Thread.Sleep(1000);
+            }
+        }
+    }
+    
+    class Alarm
+    {
+        public Alarm(Clock clock)
+        {
+            clock.Func1 += alarm;
+        }
+
+        public void alarm(string clock_time)
+        {
+            while (true)
+            {
+                string time = DateTime.Now.ToString("hh:mm:ss");
+                if(time == clock_time)
+                {
+                    Console.WriteLine($"{0}时间到了",time);
+                }
+            }
+        }
+    }
+    
+}
---
+++ homework4/GenericList/GenericList/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace GenericList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            GenericList<int> intlist = new GenericList<int>();
+            for(int x = 0; x < 10; x++)
+            {
+                intlist.Add(x);
+            }
+            int max = intlist.Head.Data;
+            int min = intlist.Head.Data;
+            intlist.Traverse(s => Console.WriteLine(s));
+            intlist.Traverse(s => max = (s > max) ? s : max);
+            intlist.Traverse(s => min = (s < min) ? s : min);
+            Console.WriteLine(max);
+            Console.WriteLine(min);
+        }
+    }
+
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if(tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+
+        public void Traverse(Action<T> func)
+        {
+            Node<T> p = head;
+            while(p != null)
+            {
+                T data = p.Data;
+                func(data);
+                p = p.Next;
+            }
+        }
+    }
+}
