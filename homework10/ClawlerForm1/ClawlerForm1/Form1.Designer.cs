9be9c48c6800287402a4aa4a50c630843c9543e6
tlssnp <70431735+tlssnp@users.noreply.github.com>
2021-05-11 22:39:56 +0800

    第十次作业

---
+++ homework10/ClawlerForm1/ClawlerForm1/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿
+namespace ClawlerForm1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnCrawl = new System.Windows.Forms.Button();
+            this.labUrl = new System.Windows.Forms.Label();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.labStatue = new System.Windows.Forms.Label();
+            this.Url = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.labStatue);
+            this.panel1.Controls.Add(this.btnCrawl);
+            this.panel1.Controls.Add(this.labUrl);
+            this.panel1.Controls.Add(this.txtUrl);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 116);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnCrawl
+            // 
+            this.btnCrawl.Location = new System.Drawing.Point(643, 54);
+            this.btnCrawl.Name = "btnCrawl";
+            this.btnCrawl.Size = new System.Drawing.Size(86, 23);
+            this.btnCrawl.TabIndex = 2;
+            this.btnCrawl.Text = "开始爬取";
+            this.btnCrawl.UseVisualStyleBackColor = true;
+            this.btnCrawl.Click += new System.EventHandler(this.btnCrawl_Click);
+            // 
+            // labUrl
+            // 
+            this.labUrl.AutoSize = true;
+            this.labUrl.Location = new System.Drawing.Point(49, 55);
+            this.labUrl.Name = "labUrl";
+            this.labUrl.Size = new System.Drawing.Size(40, 16);
+            this.labUrl.TabIndex = 1;
+            this.labUrl.Text = "URL:";
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(107, 52);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(487, 26);
+            this.txtUrl.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.dataGridView1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 116);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(800, 334);
+            this.panel2.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Url});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 53;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(800, 334);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // labStatue
+            // 
+            this.labStatue.AutoSize = true;
+            this.labStatue.Location = new System.Drawing.Point(313, 19);
+            this.labStatue.Name = "labStatue";
+            this.labStatue.Size = new System.Drawing.Size(56, 16);
+            this.labStatue.TabIndex = 3;
+            this.labStatue.Text = "label1";
+            // 
+            // Url
+            // 
+            this.Url.HeaderText = "url";
+            this.Url.MinimumWidth = 7;
+            this.Url.Name = "Url";
+            this.Url.Width = 600;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "SimpleCrawler";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label labUrl;
+        private System.Windows.Forms.Button btnCrawl;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn hostnameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn contentsDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Label labStatue;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Url;
+    }
+}
+
---
+++ homework10/ClawlerForm1/ClawlerForm1/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Crawler;
+using System.IO;
+
+namespace ClawlerForm1
+{
+    public partial class Form1 : Form
+    {
+        SimpleCrawler simpleCrawler = new SimpleCrawler();
+        public Form1()
+        {
+            InitializeComponent();
+            txtUrl.DataBindings.Add("Text", simpleCrawler, "startUrl");
+        }
+
+        private void btnCrawl_Click(object sender, EventArgs e)
+        {
+            simpleCrawler.StartCrawl();
+            for(int i = 0; i < simpleCrawler.urls.Count(); i++)
+            {
+                dataGridView1.Rows.Add(simpleCrawler.urls[i]);
+            }
+        }
+    }
+}
